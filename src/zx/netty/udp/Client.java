bcb4463bad6a1bfb8bc9643ec354885d6d697c5a
zxiang179 <zxiang248@gmail.com>
2017-05-11 13:43:37 +0800

    udp

--- src/zx/netty/udp/Client.java
+++ src/zx/netty/udp/Client.java
@@ -1,42 +1,41 @@
-package zx.netty.udp;
 
-import java.net.InetSocketAddress;
+package zx.netty.udp;
 
 import io.netty.bootstrap.Bootstrap;
 import io.netty.buffer.Unpooled;
 import io.netty.channel.Channel;
-import io.netty.channel.ChannelHandlerAdapter;
 import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.DatagramPacket;
 import io.netty.channel.socket.nio.NioDatagramChannel;
 import io.netty.util.CharsetUtil;
 
+import java.net.InetSocketAddress;
+
 
 public class Client {
-	
-	public void run(int port) throws InterruptedException{
-		NioEventLoopGroup group = new NioEventLoopGroup();
-		
-		try {
-			Bootstrap b = new Bootstrap();
-			b.group(group).channel(NioDatagramChannel.class)
-			.option(ChannelOption.SO_BROADCAST, true)
-			.handler(new ClientHandler());
-			Channel ch = b.bind(0).sync().channel();
-			//向网段内所有的及其广播UDP消息
-			ch.writeAndFlush(new DatagramPacket(Unpooled.copiedBuffer("谚语字典查询？", CharsetUtil.UTF_8),
-					new InetSocketAddress("255.255.255.255",port))).sync();
-			if(ch.closeFuture().await(15000)){
-				System.out.println("查询超时");
-			}
-		} finally{
-			group.shutdownGracefully();
-		}
-	}
-	
-	public static void main(String[] args) throws InterruptedException {
-		new Client().run(8765);
-	}
 
+    public void run(int port) throws Exception {
+		EventLoopGroup group = new NioEventLoopGroup();
+		try {
+		    Bootstrap b = new Bootstrap();
+		    b.group(group).channel(NioDatagramChannel.class)
+			    .option(ChannelOption.SO_BROADCAST, true)
+			    .handler(new ClientHandler());
+		    Channel ch = b.bind(0).sync().channel();
+		    // 鍚戠綉娈靛唴鐨勬墍鏈夋満鍣ㄥ箍鎾璘DP娑堟伅
+		    ch.writeAndFlush(new DatagramPacket(Unpooled.copiedBuffer("璋氳瀛楀吀鏌ヨ?", CharsetUtil.UTF_8), 
+		    		new InetSocketAddress("255.255.255.255", port))).sync();
+		    if (!ch.closeFuture().await(15000)) {
+		    	System.out.println("鏌ヨ瓒呮椂!");
+		    }
+		} finally {
+		    group.shutdownGracefully();
+		}
+    }
+
+    public static void main(String[] args) throws Exception {
+    	new Client().run(8765);
+    }
 }
